b7c8fb3c8dd6b2e6e090678b72f9de707954d77e
yewei <yewei_andy@163.com>
2020-12-28 16:57:32 +0800

    feat:add example[echo]

---
+++ example/echo/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"github.com/zzinno/z-mqtt-client/client"
+)
+
+func main() {
+	c := client.ZClient{}
+	err := c.New(client.Config{
+		Port:      1883,
+		Broker:    "47.100.242.110",
+		ClientID:  "testClient",
+		Username:  "dev",
+		Password:  "123123",
+		SelfTopic: "testCli",
+		CallBack: func(msg *client.RequestMsg) (*[]byte, error) {
+			fmt.Println(msg.FromTopic)
+			return nil, nil
+		},
+		Logger: nil,
+	})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	_, err = c.SendMsgAndWaitReply(client.Data{
+		Key:     []byte("222"),
+		Columns: "222",
+	}, "testCli", 10)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	c.Loop()
+}
